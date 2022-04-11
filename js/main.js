03d41d217a5c193532f310290f7964d853e4e6ef
ashik4715 <ashikurjhalak@gmail.com>
2022-04-12 01:43:52 +0600

    section 7 doing

--- js/main.js
+++ js/main.js
@@ -48,3 +48,31 @@ $(window).scroll(function() {
 });
 
 // end Project Counter
+
+// telephone addon
+$("#phoneNumber").intlTelInput({
+    utilsScript: "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/8.4.6/js/utils.js"
+});
+// end telephone addon
+
+// Contact form Validation
+(function() {
+    'use strict'
+
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.querySelectorAll('.needs-validation')
+
+    // Loop over them and prevent submission
+    Array.prototype.slice.call(forms)
+        .forEach(function(form) {
+            form.addEventListener('submit', function(event) {
+                if (!form.checkValidity()) {
+                    event.preventDefault()
+                    event.stopPropagation()
+                }
+
+                form.classList.add('was-validated')
+            }, false)
+        })
+})()
+// end Validation
